56febbdc8d437e5cb3919bfc670dc496cf2f9171
Keenan Alves <keenan.alves@gmail.com>
2019-08-28 18:55:59 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(numbers)
   counter = 0    
  new_numbers = []
   
-  while counter != numbers.size do
+  while counter >= numbers.size do
    new_numbers << counter ** 2 
     counter += 1
   end
